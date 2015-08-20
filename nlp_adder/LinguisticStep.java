e4dafcb4cf562a4db93afbe67a34bd7d0df253dc
Mukund Muralikrishnan <mukund@healthifyme.com>
2015-08-20 15:57:57 +0530

    very minor code change

--- nlp_adder/LinguisticStep.java
+++ nlp_adder/LinguisticStep.java
@@ -1,17 +1,17 @@
 package nlp_adder;
 
 public class LinguisticStep {
-	boolean isQuestion;
-	boolean aggregator;
-	boolean comparator;
-	boolean difference;
-	String keyword;
-	String owner1;
-	String owner2;
-	String entityName;
-	String entityValue;
-	String tense;
-	String nounQual;
-	String verbQual;
-	String procedureName;
+    boolean isQuestion;
+    boolean aggregator;
+    boolean comparator;
+    boolean difference;
+    String keyword;
+    String owner1;
+    String owner2;
+    String entityName;
+    String entityValue;
+    String tense;
+    String nounQual;
+    String verbQual;
+    String procedureName;
 }
